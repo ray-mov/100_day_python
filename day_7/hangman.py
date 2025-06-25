2938984194c2f9e1240a2734a7450c026e244e73
ray-mov <monovray@gmail.com>
2025-06-25 12:30:42 +0530

    hangman game completed

--- day_7/hangman.py
+++ day_7/hangman.py
@@ -2,36 +2,43 @@ import random
 
 words = ["apple", "banana", "orange", "grape", "melon", "kiwi", "mango", "pear", "peach", "plum"]
 life = 5
-
-
 chosen_word = random.choice(words)
 word_len = len(chosen_word)
-char_left = word_len
+display_word = ["_"]* word_len
 
-temp_spaces = ""
+print(" ".join(display_word))
 
-for num in range(0,word_len):
-  temp_spaces+=" _ "
+guessed_letters = set()
 
-print(temp_spaces)
+while "_" in display_word and life:
+   guess = input("Please guess a letter : ").lower()
+   
+   if not guess.isalpha() and len(guess) != 1:
+     print("Please Enter a valid character ! ")
+     continue
+   
+   if guess in guessed_letters:
+     print('You already choose that  word ')
+     continue
+   
+   guessed_letters.add(guess)
+   
+   if guess in chosen_word:
+     print("You guessed correctly")
+     for i in range(word_len):
+       if chosen_word[i] == guess:
+         display_word[i] = guess
+   else:
+     life -=1
+     print(f"Wrong guess. Lives left: {life}")
 
-while char_left and life:
-    guess = input("Please guess a letter : ").lower()
-    if not guess.isalpha():
-      print("Please Enter a valid character ! ")
-      continue
-    for num in range(0,word_len):
-      if chosen_word[num] == guess:
-        print("You guess correct")
-        char_left-=1
-        temp_spaces[num] =" "+ guess + " "
-      else:
-        print("Oops Wring Guess")
-        life-=1
-        print(f"Life left {life}")
-    print(temp_spaces)
-      
-         
+   print(" ".join(display_word))
+     
+   
   
-    
-    
+
+# Game over messages
+if "_" not in display_word:
+    print(f"\n🎉 Congrats! You guessed the word: {chosen_word}")
+else:
+    print(f"\n💀 Game Over! The word was: {chosen_word}")
