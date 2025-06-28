a634f0a0d2a0aa2a316057b80aa4f72bb2706acc
ray-mov <monovray@gmail.com>
2025-06-28 12:07:24 +0530

    love_calculator

--- day_8/love_calculator.py
+++ day_8/love_calculator.py
@@ -1,22 +1,29 @@
 def calculate_love_score(name_1, name_2):
-  
-    
-    check_name =  (name_1 + name_2).lower().strip()
-    
-    num_true = 0
-    num_love = 0
-    
-    for char in "true":
-        if char in name_1:
-            num_true+=1
+    name = (name_1 + name_2).lower().replace(" ","")
+    score_1 = 0
+    score_2 = 0
 
-     
-    for char in "love":
-        if char in name_1:
-            num_true+=1
+    for char in name:
+        if char == "t":
+            score_1+=1
+        elif char == 'r':
+            score_1+=1
+        elif char == 'u':
+            score_1+=1
+        elif char == 'e':
+            score_1+=1
+            score_2+=1
+        elif char == 'l':
+            score_2+=1
+        elif char == 'o':
+            score_2+=1
+        elif char == 'v':
+            score_2+=1
+        else:
+            continue
+    
+    print(f"{score_1}{score_2}")
             
-    
-    
     
     
 calculate_love_score("Kanye West", "Kim Kardashian")
