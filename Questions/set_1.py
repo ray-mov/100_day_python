5ba24a2db9340a3823bdb8b2f0d71b803ca204e8
ray-mov <monovray@gmail.com>
2025-06-29 10:56:15 +0530

    q

---
+++ Questions/set_1.py
@@ -0,0 +1,20 @@
+# Q1: print simon and Aryan
+text = "John,Simon,Aryan"
+print(text[-11:-6])
+print(text[-5:])
+
+# Q2: output
+print(max("what are you"))   # 'y' has the highest ASCII value (121) among all characters
+
+# Q3: output
+example="hello" 
+print(example.count('l'))
+
+
+# Q4: output
+print(chr(ord('A')))
+
+# ord('A') Returns the Unicode code point of the character 'A'.
+# 'A' → 65
+# chr(65) Converts the Unicode code point 65 back into a character.
+# 65 → 'A'
